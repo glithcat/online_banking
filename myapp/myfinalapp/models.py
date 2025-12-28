1d4b288ca3fa514e4bf1788d0e137affb164a7cd
glithcat <155246470+glithcat@users.noreply.github.com>
2025-12-28 11:14:56 +0000

    зроблено неробочій прототип кнопки та модель користувача та кредита. також не робочі

--- myapp/myfinalapp/models.py
+++ myapp/myfinalapp/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
-#class User(models):
-#    a =
+#basic user model
+class User(models):
+    user_name = models.CharField(unique=False, max_length=10)
+    user_surname = models.CharField(unique=False, max_length=10)
+    user_birthday = models.DateField() #YYYY:MM:DD
+    user_balance = models.SmallIntegerField()
+    unique_id = models.IntegerField(primary_key=True)
+    #TODO this :user_credits =
+
+class credit(models):
+    sum_borrowed = models.SmallIntegerField()
+    interest = models.SmallIntegerField()
